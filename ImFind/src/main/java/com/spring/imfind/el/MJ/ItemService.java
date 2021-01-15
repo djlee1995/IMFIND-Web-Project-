d9eaf58cdbf3defc815fc6715df4cee99f24e7b6
LeeDongJun <72503526+djlee1995@users.noreply.github.com>
2021-01-15 11:29:48 +0900

    1차 merge 준비

--- ImFind/src/main/java/com/spring/imfind/el/MJ/ItemService.java
+++
@@ -1,9 +0,0 @@
-package com.spring.imfind.el.MJ;
-
-import java.util.List;
-
-public interface ItemService {
-			public List<ItemVO> getItemservice(ItemVO vo);
-			public List<ItemVO> getItemservice(String lost_Title);
-		
-}
