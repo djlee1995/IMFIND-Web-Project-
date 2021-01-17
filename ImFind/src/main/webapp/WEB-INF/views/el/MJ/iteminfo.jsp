3312c400195f85a38d58062f042b40bc278caa3f
LeeDongJun <72503526+djlee1995@users.noreply.github.com>
2021-01-17 20:11:18 +0900

    게시물상세조회 페이지(세션 id 값마다 수정버튼 변화)

--- ImFind/src/main/webapp/WEB-INF/views/el/MJ/iteminfo.jsp
+++ ImFind/src/main/webapp/WEB-INF/views/el/MJ/iteminfo.jsp
@@ -7,13 +7,76 @@
 <title>Insert title here</title>
 </head>
 <body>
+
+
+<!-- var postnum;
+	var title;
+	var content;
+	var redate;
+	var recount;
+	var file;
+	var pay;
+	var item;
+	var loc;
+	var lostdate;
+	var id; -->
 <table id="data_output" align="center">
+  <tr id="update" align="right">
+    
+  </tr>
+  <tr>
+    <th rowspan="6" colspan="3" id="file">
+     
+    </th>
+    <th colspan="2" id="title"></th>
+  </tr>
+  <tr>
+    <th>사례금</th>
+    <td id="pay"></td>
+  </tr>
+  <tr>
+    <th>분실자</th>
+    <td id="id"></td>
+  </tr>
+  <tr>
+    <th>분실날짜</th>
+    <td id="lostdate">2020.10.13</td>
+  </tr>
+  <tr>
+    <th>분실위치</th>
+    <td id="loc"></td>
+  </tr>
+
+  <tr>
+    <th colspan="2"><button style="width:100%;" type="button" name="button">연락하기</button></th>
+  </tr>
+  <tr height="30px">
+  </tr>
+  <tr>
+    <th colspan="5" align="left">분실정보</th>
+  </tr>
+  <tr>
+    <td colspan="5" id="content">
+    </td>
+  </tr>
+  <tr>
+	<td colspan="5" align="center">
+    <div id="map" style="width:500px; height:500px; margin-top:70px;"></div>
+    </td>  
+   </tr>
+  
 </table>
-<script src="https://code.jquery.com/jquery-3.5.1.js"></script> 
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.js" ></script> 
 <script>
-var lost_PostNum = <%= request.getParameter("lost_PostNum") %>;
+var lost_PostNum = <%= request.getParameter("lost_PostNum") %>
 console.log(lost_PostNum)
+var loginUser=<%=(String)session.getAttribute("loginUser")%>
+var kakaoLoginUser <%=(String)session.getAttribute("kakaoLoginUser")%>
+
 </script>
+<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=5e9646f261380e768a278eb16f4f6768&libraries=services"></script>
 <script src="${pageContext.request.contextPath}/resources/el/js/iteminfo.js"></script>
 <jsp:include page="../EJ/comment.jsp" flush="true" />
 
--- ImFind/src/main/webapp/resources/el/js/iteminfo.js
+++ ImFind/src/main/webapp/resources/el/js/iteminfo.js
@@ -1,31 +1,35 @@
 
 $(document).ready(function() {
+	var loc;
 	$.ajax({
 		url :'datainfo.do',
 		//type : 'POST',
 		data : {'lost_PostNum': lost_PostNum },
+		async:false,
 		contentType: 'application/x-www-form-urlencoded;charset=utf-8',
 		//dataType:'json',
 		success: function(data){
-			$.each(data, function(index,item){
-				console.log(data)
-				var output = '';
-				output +='<tr style="font-size: large;"><td>'+ item.lost_PostNum + '</td></tr>';
-				output +='<tr style="font-size: large;"><td>'+ item.lost_Title + '</td></tr>';
-				output +='<tr style="font-size: large;"><td>'+ item.lost_Content + '</td></tr>';
-				output +='<tr style="font-size: large;"><td>'+ item.lost_Re_Date + '</td></tr>';
-				output +='<tr style="font-size: large;"><td>'+ item.lost_Re_Count + '</td></tr>';
-				output +='<tr style="font-size: large;"><td>'+ item.lost_Content + '</td></tr>';
-				output +='<tr><td><img width="300px"; height="300px"; src="'+ item.lost_Up_File + '"></td></tr>';
-				output +='<tr style="font-size: large;"><td>'+ item.lost_Pay + '</td></tr>';
-				output +='<tr style="font-size: large;"><td>'+ item.lost_Item + '</td></tr>';
-				output +='<tr style="font-size: large;"><td>'+ item.lost_Loc + '</td></tr>';
-				output +='<tr style="font-size: large;"><td>'+ item.lost_Date + '</td></tr>';
-				output +='<tr style="font-size: large;"><td>'+ item.id + '</td></tr>';
-				output +='<tr style="font-size: large;"><td>'+ item.contact + '</td></tr>';
-				$('#data_output').append(output);
-	
-			});
+			console.log(loginUser)
+			if($.trim(loginUser)==(data[0].id)){
+				$('#update').html('<th colspan="5"><input type="button" class="updateBtn" value="수정"></th>');
+			}
+			else if($.trim(kakaoLoginUser)==(data[0].id)){
+				$('#update').html('<th colspan="5"><input type="button" class="updateBtn" value="수정"></th>');
+			}
+			console.log()
+				loc=data[0].lost_Loc
+				//$('#postnum').text(data[0].lost_PostNum);
+				$('#title').text(data[0].lost_Title);
+				$('#content').text(data[0].lost_Content);
+				//$('#postnum').text(data[0].lost_Re_Date);
+				//$('#postnum').text(data[0].lost_Re_Count);
+				$('#file').html('<img height="50%" width="70%" src="'+data[0].lost_Up_File+'">');
+				$('#pay').text(data[0].lost_Pay);
+				//$('#postnum').text(data[0].lost_Item);
+				$('#loc').text(data[0].lost_Loc);
+				$('#lostdate').text(moment(data[0].lost_Date).format('YYYY-MM-DD'));
+				$('#id').text(data[0].id);
+				//$('#postnum').text(data[0].contact);
 			},
 			
 		error : function() {
@@ -37,4 +41,40 @@ $(document).ready(function() {
 
 
 	});
+	console.log(loc)
+	
+	var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+    mapOption = {
+        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+    };  
+
+var map = new kakao.maps.Map(mapContainer, mapOption); 
+
+// 장소 검색 객체를 생성합니다
+var ps = new kakao.maps.services.Places();
+// 키워드로 장소를 검색합니다
+ps.keywordSearch(loc, placesSearchCB);
+// 키워드 검색 완료 시 호출되는 콜백함수 입니다
+function placesSearchCB (data, status, pagination) {
+	 if (status === kakao.maps.services.Status.OK) {
+		 
+		 // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+		 // LatLngBounds 객체에 좌표를 추가합니다
+		 var bounds = new kakao.maps.LatLngBounds();
+		 for (var i=0; i<data.length; i++) {
+			 bounds.extend(new kakao.maps.LatLng(data[0].y, data[0].x));
+			// 결과값으로 받은 위치를 마커로 표시합니다
+		        var marker = new kakao.maps.Marker({
+		            map: map,
+		            position: new kakao.maps.LatLng(data[0].y, data[0].x)
+		        });
+		 }
+		 // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+		 map.setBounds(bounds);
+	 }
+}
+	
+	
+	
 });
